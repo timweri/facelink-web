17a1859ca655b5587121deb5c5b6b224b66a321e
Quí Nguyễn Đình <qui.nguyen@carbon8.com>
2018-06-12 16:51:39 +0700

    tạo module fullpage vs add content

---
+++ src/html/data/modules/frame1.js
@@ -0,0 +1,8 @@
+module.exports = {
+  subhead: 'Everything you need, nothing you don’t.',
+  title: 'Collagen <span>L-Glutamine</span> Creatine',
+  button_text: 'SHOP NOW',
+  button_link: '#',
+  image: 'images/girl-image.png',
+  image_product: ''
+}
