c03abfd3ea1d723df377d9f195cbd4c25b2b3c8f
Quí Nguyễn Đình <qui.nguyen@carbon8.com>
2018-06-12 17:48:06 +0700

    update icon

---
+++ src/html/data/modules/frame2.js
@@ -0,0 +1,26 @@
+module.exports = {
+  subhead: 'The first all-natural daily supplement for...',
+  title: 'Performance, Recovery & Results',
+  list_icon: [
+    {
+      icon: 'images/icon1.svg',
+      icon_title: 'All-Natural no chemicALs'
+    },
+    {
+      icon: '',
+      icon_title: 'Non-GMO'
+    },
+    {
+      icon: '',
+      icon_title: 'No Artificial flavoring or sweetners'
+    },
+    {
+      icon: '',
+      icon_title: 'A Colorado Brand'
+    },
+    {
+      icon: '',
+      icon_title: 'Mixed and packaged in the USA'
+    }
+  ]
+}
