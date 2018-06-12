c1e8ec057b8b92953c44589cf1a30cf2b264e315
timweri <ng.duc.tahn@gmail.com>
2018-06-12 16:45:31 +0700

    Complete banner module

--- src/html/data/global.js
+++ src/html/data/global.js
@@ -1,4 +1,9 @@
 module.exports = {
   'title': 'Frontend starter template',
-  banner: require('./modules/banner')
+  banner_shop: require('./modules/banner-shop'),
+  banner_benefits: require('./modules/banner-benefits'),
+  banner_contact: require('./modules/banner-contact'),
+  banner_faq: require('./modules/faq'),
+  banner_media: require('./modules/banner-media'),
+  banner_story: require('./modules/banner-story')
 }
---
+++ src/html/data/modules/banner-benefits.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: 'Benefits',
+    background_image: 'images/banner-bg.png',
+    position_background_image: 'top center',
+    icon_green: true,
+    product_image: '',
+    button_text: '',
+    button_link: ''
+  }
