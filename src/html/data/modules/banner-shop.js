41ac7bb6f23d587fe60c4a199201759844b6582c
timweri <ng.duc.tahn@gmail.com>
2018-06-13 08:40:20 +0700

    Use .jpg instead of .png

--- src/html/data/modules/banner-shop.js
+++ src/html/data/modules/banner-shop.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'Everything you need, nothing you don\'t',
-    background_image: 'images/banner-bg.png',
+    background_image: 'images/banner-shop-bg.jpg',
     position_background_image: 'top center',
     icon_green: true,
     product_image: 'images/product.png',
