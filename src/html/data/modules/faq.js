7f560bdc28356c9ddd3abb48dc18b51c168520fc
timweri <ng.duc.tahn@gmail.com>
2018-06-15 17:23:00 +0700

    Longer text

--- src/html/data/modules/faq.js
+++ src/html/data/modules/faq.js
@@ -9,10 +9,10 @@ module.exports = {
         },
         {
           question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          answer: 'Lorem ipsum dolor sit amet, ne justo laoreet percipit nam. Mel viris elaboraret no. Ut has wisi qualisque, clita epicuri nam ex. Id omnes omnesque duo, magna incorrupte te mea. Te nec dicunt utamur, ut oratio doctus petentium nam. Noster habemus nostrum eum no.'
+          answer: 'Lorem ipsum dolor sit amet, ne justo laoreet percipit nam. Mel viris elaboraret no. Ut has wisi qualisque, clita epicuri nam ex. Id omnes omnesque duo, magna incorrupte te mea. Te nec dicunt utamur, ut oratio doctus petentium nam. Noster habemus nostrum eum no.Lorem ipsum dolor sit amet, ne justo laoreet percipit nam. Mel viris elaboraret no. Ut has wisi qualisque, clita epicuri nam ex. Id omnes omnesque duo, magna incorrupte te mea. Te nec dicunt utamur, ut oratio doctus petentium nam. Noster habemus nostrum eum no.'
         },
         {
-          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           answer: 'Lorem ipsum dolor sit amet, ne justo laoreet percipit nam. Mel viris elaboraret no. Ut has wisi qualisque, clita epicuri nam ex. Id omnes omnesque duo, magna incorrupte te mea. Te nec dicunt utamur, ut oratio doctus petentium nam. Noster habemus nostrum eum no.'
         },
         {
