cbd01b534f368cae6c74a81b425a77490da20ed0
Quí Nguyễn Đình <qui.nguyen@carbon8.com>
2018-06-17 20:12:58 +0700

    update html frame 3 vs frame4 page home

--- src/html/data/index.js
+++ src/html/data/index.js
@@ -3,5 +3,6 @@ module.exports = {
   'footer': true,
   frame1: require('./modules/frame1'),
   frame2: require('./modules/frame2'),
-  frame3: require('./modules/frame3')
+  frame3: require('./modules/frame3'),
+  frame4: require('./modules/frame4')
 }
---
+++ src/html/data/modules/frame4.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Build a better you in',
+  optimize: [
+    {
+      title: 'Optimize your Workout'
+    },
+    {
+      title: 'Build Lean Muscle'
+    },
+    {
+      title: 'Reduce Recovery Time'
+    }
+  ],
+  image_product: 'images/product.png'
+}
