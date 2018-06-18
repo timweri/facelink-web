ea878223b3713d86c2798e9825dc83335e4f7e75
timweri <ng.duc.tahn@gmail.com>
2018-06-18 17:10:24 +0700

    module featured-news-post

--- src/html/data/modules/featured-news-post.js
+++ src/html/data/modules/featured-news-post.js
@@ -1,5 +1,6 @@
 module.exports = {
-  logo: '',
+  logo: 'images/abc-logo.svg',
   title: 'Full width no image',
-  body: 'Headline of the article or quote of the mention as the headline'
+  body: 'Headline of the article or quote of the mention as the headline',
+  link: '#'
 }
