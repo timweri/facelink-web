f6c981fddb60ccfe7900d068b1c8ed17fb61eff8
timweri <ng.duc.tahn@gmail.com>
2018-06-18 16:29:36 +0700

    Remove relative path

--- src/html/data/modules/content-benefits.js
+++ src/html/data/modules/content-benefits.js
@@ -5,5 +5,5 @@ module.exports = {
   <p>That’s where Briq Comes in. We train and we know what it takes to reach the top. We wanted to get away from the rows of tubs and the complicated mixing and measuring that went along with so many nutrition plans.</p>\
   <p>So we created a simple daily supplement that was easy to use, all natural and made solely from three powerful ingredients: L-glutamin, collagen and creatine.</p>\
   <p>If you are willing to put in the work, we have the building materials you need.</p>',
-  image: '../images/benefit-content-image.jpg'
+  image: 'images/benefit-content-image.jpg'
 }
---
+++ src/html/data/modules/featured-news-post.js
@@ -0,0 +1,5 @@
+module.exports = {
+  logo: '',
+  title: 'Full width no image',
+  body: 'Headline of the article or quote of the mention as the headline'
+}
