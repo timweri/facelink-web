88225dbdd1079fa807f3f131db8dad3e0620bc38
Quí Nguyễn Đình <qui.nguyen@carbon8.com>
2018-06-19 09:28:36 +0700

    page faq

--- src/html/data/global.js
+++ src/html/data/global.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'title': 'Frontend starter template',
+  'title': 'Universal',
   banner_shop: require('./modules/banner-shop'),
   banner_benefits: require('./modules/banner-benefits'),
   banner_contact: require('./modules/banner-contact'),
-  banner_faq: require('./modules/faq'),
+  banner_faq: require('./modules/banner-faq'),
   banner_media: require('./modules/banner-media'),
   banner_story: require('./modules/banner-story'),
   cta: require('./modules/cta'),
