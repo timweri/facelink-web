e437b6e3221cefc7dae7fb17bfa3ab15011ee2fe
Quí Nguyễn Đình <qui.nguyen@carbon8.com>
2018-06-19 15:29:00 +0700

    rap module vào page press.html

--- src/html/data/modules/news-feed.js
+++ src/html/data/modules/news-feed.js
@@ -4,21 +4,27 @@ module.exports = {
       image: 'images/news-feed-image.jpg',
       logo: 'images/abc-logo.svg',
       title: 'Publication with image',
-      body: 'Headline of the article of quote of the mention as the headline',
+      body: `<p>Headline of the article of quote of the mention as the headline</p>
+        <p><a class="btn btn-primary" href="#">View article</a></p>
+      `
     },
     {
       image: 'images/news-feed-image.jpg',
       logo: 'images/abc-logo.svg',
       title: 'Publication title',
-      body: 'Headline of the article of quote of the mention as the headline',
+      body: `<p>Headline of the article of quote of the mention as the headline</p>
+        <p><a class="btn btn-primary" href="#">View article</a></p>
+      `
     },
     {
       image: 'images/news-feed-image.jpg',
       logo: 'images/abc-logo.svg',
       title: 'Publication with image',
-      body: 'Headline of the article of quote of the mention as the headline',
+      body: `<p>Headline of the article of quote of the mention as the headline</p>
+        <p><a class="btn btn-primary" href="#">View article</a></p>
+      `
     }
   ],
-  quote: '<h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</h2>\
-  <p>By: Doug Fancy on Friday February 4<sup>th</sup> 2016</p>'
+  quote: `<h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</h2>
+  <p>By: Doug Fancy on Friday February 4<sup>th</sup> 2016</p>`
 }
--- src/html/data/modules/tab-benefits.js
+++ src/html/data/modules/tab-benefits.js
@@ -1,7 +1,7 @@
 module.exports = {
   tabs: [
     {
-      title: 'Reduce recovery time Reduce recovery time',
+      title: 'Reduce recovery time',
       content: '<p><img src="images/benefit-tab-compound.jpg" alt="Compound"><p>\
       <h4>Creatine Monohydrate (1): </h4>\
       <p>Creatine Monohydrate: Most effective derivative of all creatines. Creatine enhances the bodies capacity to perform high intensity activity and assists in greater muscle size and performance gains as a result. Creatine Phosphate (creatines high energy molecule form, stored within the cells) is used to supply the type 11b muscle fibers (fast-twitch high-glycolytic; the muscles that get largest in size) with immediate energy, ensuring these muscles do not prematurely fatigue.</p>'
