1da58c077eef6d9ca139a44d1f91dc4cc3f88b47
timweri <ng.duc.tahn@gmail.com>
2018-06-20 09:43:02 +0700

    shop-product module

---
+++ src/html/data/modules/shop-product.js
@@ -0,0 +1,18 @@
+module.exports = {
+  products: [
+    {
+      title: 'Recovery powder',
+      image: 'images/shop-product-image.jpg',
+      btn_text: 'Buy now',
+      btn_link: '#',
+      overlay_image: 'images/fruit.png'
+    },
+    {
+      title: 'Briq gear',
+      image: 'images/shop-gear-image.jpg',
+      btn_text: 'View all gear',
+      btn_link: '#',
+      overlay_image: 'images/cap.png'
+    }
+  ]
+}
