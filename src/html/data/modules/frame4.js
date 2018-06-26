13e1dbe3b01929b4bdfc00f02c0ce55ee6940c71
timweri <ng.duc.tahn@gmail.com>
2018-06-26 10:07:57 +0700

    Refactor: Editable in CMS as a list

--- src/html/data/modules/frame4.js
+++ src/html/data/modules/frame4.js
@@ -1,15 +1,16 @@
 module.exports = {
   title: 'Build a better you in',
-  optimize: [
-    {
-      title: 'Optimize your Workout'
-    },
-    {
-      title: 'Build Lean Muscle'
-    },
-    {
-      title: 'Reduce Recovery Time'
-    }
-  ],
+  block_list: [1, 2, 3],
+  bullet_list: `<ul>
+    <li>
+      Optimize your workout
+    </li>
+    <li>
+      Build lean muscle
+    </li>
+    <li>
+      Reduce recovery time
+    </li>
+  </ul>`,
   image_product: 'images/product.png'
 }
