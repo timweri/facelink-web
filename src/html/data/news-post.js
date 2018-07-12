c520181fd2f1f37a4a0d8c94d67a65bc2a6a1dd2
timweri <ng.duc.tahn@gmail.com>
2018-07-12 14:09:53 +0700

    Change sample news-post

--- src/html/data/news-post.js
+++ src/html/data/news-post.js
@@ -2,19 +2,18 @@ module.exports = {
   'title': 'News',
   banner: require('./modules/banner-media'),
   content_editor: {
-    body: `<div class="center-content text-center" style="opacity: 1; transform: none;">
-    <img src="images/banner-bg-shop.jpg">
-    <h2 style="text-align: center;">Map Your Future</h2>
-    <p style="text-align: left;">We asked ourselves one critical question when thinking about how best to grow Concept3D: What can we do to make this a place where we would want to work?</p>
-    <h3 style="text-align: left;">Our answer?</h3>
-    <ol style="text-align: left;">
-    <li style="text-align: left;">Make it a company where you have meaningful work, using awesome challenging technologies, that truly helps clients succeed</li>
-    <li style="text-align: left;">Ensure mutual respect among everyone, regardless of their title</li>
-    <li style="text-align: left;">Prove that there’s a real commitment to work/life balance</li>
-    </ol><p style="text-align: left;">So, that’s what we’ve done. We’ve made sure that the time you dedicate to your profession includes work you look forward to, a team that looks out for each other and, of course, ping-pong, puzzles and beer on tap – whether you’re in our Denver or Eden Prairie office.</p>
-    <p style="text-align: left;">This is a two – way relationship. We invest in you by providing 100% health care coverage, a generous vacation and personal time off plan, and an ownership opportunity in the company. We’re truly all in it together.</p>
-    <p style="text-align: left;">If you are looking to change the way business is done by delivering innovation at the intersection of place, data, and 3D – we want to hear from you.</p>
-    <p style="text-align: left;"><em>Gordon Boyes<br> </em>CEO</p>
+    body: `<div>
+    <h2>Map Your Future</h2>
+    <p>We asked ourselves one critical question when thinking about how best to grow Concept3D: What can we do to make this a place where we would want to work?</p>
+    <h3>Our answer?</h3>
+    <ol>
+    <li>Make it a company where you have meaningful work, using awesome challenging technologies, that truly helps clients succeed</li>
+    <li>Ensure mutual respect among everyone, regardless of their title</li>
+    <li>Prove that there’s a real commitment to work/life balance</li>
+    </ol><p>So, that’s what we’ve done. We’ve made sure that the time you dedicate to your profession includes work you look forward to, a team that looks out for each other and, of course, ping-pong, puzzles and beer on tap – whether you’re in our Denver or Eden Prairie office.</p>
+    <p>This is a two – way relationship. We invest in you by providing 100% health care coverage, a generous vacation and personal time off plan, and an ownership opportunity in the company. We’re truly all in it together.</p>
+    <p>If you are looking to change the way business is done by delivering innovation at the intersection of place, data, and 3D – we want to hear from you.</p>
+    <p><em>Gordon Boyes<br> </em>CEO</p>
     </div>`
   }
 }
